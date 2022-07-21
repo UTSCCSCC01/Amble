9ff83a2189e404d9bc2852249128eb1d3a8ea235
Yashank Bhola <64089101+yashankxy@users.noreply.github.com>
2022-07-21 19:48:08 -0400

    handle request finished
    Co-authored-by: Ser0n  <Ser0n-ath@users.noreply.github.com>

--- ApiGateway/src/main/java/ca/utoronto/utm/mcs/RequestRouter.java
+++ ApiGateway/src/main/java/ca/utoronto/utm/mcs/RequestRouter.java
@@ -28,6 +28,114 @@ public class RequestRouter implements HttpHandler {
 
 	@Override
 	public void handle(HttpExchange r) throws IOException {
-        // TODO
-	}
+                try {
+                        String uri = r.getRequestURI().toString();
+                        String[] uriParts = uri.split("/");
+                        if (uriParts[0].equals("") && uriParts[1].equals("location") ){
+                                  switch(r.getRequestMethod()){
+                                        case "GET":
+                                                if(uriParts[2].equals(":uid")){
+                                                        String response = "payload";
+                                                        r.sendResponseHeaders(200, response.length());
+                                                        OutputStream os = r.getResponseBody();
+                                                        os.write(response.getBytes());
+                                                        os.close();
+                                                }
+                                                if(uriParts[2].equals("nearbyDriver")){
+
+                                                }
+                                                if(uriParts[2].equals("navigation")){
+
+                                                }
+                                                System.out.println("Get the request");
+                                        case "PUT":
+                                                if(uriParts[2].equals("user")){
+
+                                                }
+                                                if(uriParts[2].equals("road")){
+
+                                                }
+                                        case "POST":
+                                                if(uriParts[2].equals("hasRoute")){
+
+                                                }
+                                        case "PATCH":
+                                                if(uriParts[2].equals(":uid")){
+
+                                                }
+                                        case "DELETE":
+                                                if(uriParts[2].equals("user")){
+
+                                                }
+                                                if(uriParts[2].equals("route")){
+
+                                                }
+                                        default:
+                                                r.sendResponseHeaders(500, -1);
+                                                return; 
+
+                                  }      
+                        }
+                        else if (uriParts[0].equals("") && uriParts[1].equals("trip") ){
+                                  switch(r.getRequestMethod()){
+                                        case "GET":
+                                                if(uriParts[2].equals("passenger")){
+
+                                                }
+                                                if(uriParts[2].equals("driver")){
+
+                                                }
+                                                if(uriParts[2].equals("driverTime")){
+
+                                                }
+                                        case "POST":
+                                                if(uriParts[2].equals("confirm")){
+
+                                                }
+                                                if(uriParts[2].equals("request")){
+
+                                                }
+                                        case "PATCH":
+                                                if(uriParts[2].equals(":_id")){
+
+                                                }
+                                        default:
+                                                r.sendResponseHeaders(500, -1);
+                                                return; 
+
+
+                                  }      
+                        }
+                        else if (uriParts[0].equals("") && uriParts[1].equals("user") ){
+                                  switch(r.getRequestMethod()){
+                                        case "GET":
+                                                if(uriParts[2].equals(":uid")){
+
+                                                }
+                                        case "POST":
+                                                if(uriParts[2].equals("register")){
+
+                                                }
+                                                if(uriParts[2].equals("login")){
+
+                                                }
+                                        case "PATCH":
+                                                if(uriParts[2].equals(":uid")){
+
+                                                }
+                                        default:
+                                                r.sendResponseHeaders(500, -1);
+                                                return; 
+
+                                  }      
+                        }
+                        else{
+                                r.sendResponseHeaders(500, -1);
+                                return;  
+                        }
+                }catch(Exception e){
+                        r.sendResponseHeaders(500, -1);
+                        e.printStackTrace();
+                }
+        }
 }
